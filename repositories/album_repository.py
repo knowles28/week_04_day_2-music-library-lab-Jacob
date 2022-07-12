361bcdcca971bb651fab047aa5268726b8ee19d3
Jack Knowles <jackmhknowles@gmail.com>
2022-07-12 16:23:28 +0100

    started extensions

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -40,3 +40,15 @@ def select_all():
         album = Album(row['title'], row['genre'], artist, row['id'])
         albums.append(album)
     return albums
+
+def select_by_artist(artist):
+    albums = []
+    
+    sql = "SELECT * FROM albums WHERE artist_id = %s"
+    values = [artist.id]
+    results = run_sql(sql, values)
+    
+    for row in results:
+        album = Album(row['title'], row['genre'], artist, row['id'])
+        albums.append(album)
+    return albums
